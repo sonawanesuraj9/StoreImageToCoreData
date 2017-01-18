5c9d45d8ab64d480ba51499c204522f5b2b231a8
Pramod Shirsath <pramods@supraint.com>
2017-01-18 11:37:21 +0530

    Initial Commit

---
+++ saveImageToCoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  saveImageToCoreData
+//
+//  Created by Suraj MAC3 on 18/01/17.
+//  Copyright © 2017 Suraj MAC3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
